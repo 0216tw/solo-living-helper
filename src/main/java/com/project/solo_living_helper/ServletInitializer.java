db2101975c3c341abc1421cfb5d0375df33ab97b
xodn__love@naver.com <xodn__love@naver.com>
2024-06-29 01:23:49 +0900

    프로젝트 환경 초기 구축

---
+++ src/main/java/com/project/solo_living_helper/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.project.solo_living_helper;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SoloLivingHelperApplication.class);
+	}
+
+}
---
+++ src/main/java/com/project/solo_living_helper/SoloLivingHelperApplication.java
@@ -0,0 +1,13 @@
+package com.project.solo_living_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloLivingHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloLivingHelperApplication.class, args);
+	}
+
+}
